9ec9a79f576e19663913384bd25bb6a2251c446b
peytons <peytons@18058476-d1b1-c38b-399d-f053f540a573>
2010-10-12 15:50:36 +0000

    first commit

---
+++ src/twentyfiveacross/w4156/HelloWorld.java
@@ -0,0 +1,12 @@
+package twentyfiveacross.w4156;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+			System.out.println("Hello World!");
+	}
+
+}
